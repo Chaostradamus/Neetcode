abac4db8b7c81739e539cd50dc78f5ca2da6cf84
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2024-08-31 14:12:56 -0400

    longest index between repeating characters

---
+++ arrays_hashing/1624_largest_substring_between_two_equal_characters.py
@@ -0,0 +1,50 @@
+class Solution:
+    def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+      res = -1
+
+      for i in range(len(s)):
+        for j in range(i+1, len(s)):
+          if s[i] == s[j]:
+            length = (j-i) - 1
+            res = max(length, res)
+      return res
+    
+
+    # my solutio is double for loops sliding a window from each i to end of string until we find a match and recording the len
+    # return -1 if not found or max of res and whatever is found
+
+
+
+
+# 1624. Largest Substring Between Two Equal Characters
+# Easy
+# Topics
+# Companies
+# Hint
+# Given a string s, return the length of the longest substring between two equal characters, excluding the two characters. If there is no such substring return -1.
+
+# A substring is a contiguous sequence of characters within a string.
+
+ 
+
+# Example 1:
+
+# Input: s = "aa"
+# Output: 0
+# Explanation: The optimal substring here is an empty substring between the two 'a's.
+# Example 2:
+
+# Input: s = "abca"
+# Output: 2
+# Explanation: The optimal substring here is "bc".
+# Example 3:
+
+# Input: s = "cbzxy"
+# Output: -1
+# Explanation: There are no characters that appear twice in s.
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 300
+# s contains only lowercase English letters.
