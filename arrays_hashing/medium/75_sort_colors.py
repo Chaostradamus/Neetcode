a2de3fbd028e0749843b9bf169ca364138a3731f
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2024-10-01 18:23:51 -0400

    sort colors done

--- arrays_hashing/medium/75_sort_colors.py
+++ arrays_hashing/medium/75_sort_colors.py
@@ -1,3 +1,36 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        numCount = {0:0, 1:0, 2:0}
+        for n in nums:
+          if n in numCount:
+            numCount[n] += 1
+          else:
+            numCount[n] = 1
+        i = 0
+
+        if len(nums) <= 1:
+          return
+
+        for n in numCount:
+          b = numCount[n]
+          while b > 0:
+            nums[i] = n
+            i +=1
+            b -= 1
+
+
+# 0(n) space and time because of hashmap and one pass trough nums array
+# populate a hard coded hashpmap that already contains 0 to 3 with nums where element is key and value = count
+# for every element in a now sorted numCount hashmap
+# keep an i for len  and boundary check
+# keep a count for every element
+#  change nums[i] to current element and decrement count and increase i pointer
+# at the end every element with a count will be used to fill in the current position of nums while the count decrements
+
+
 # 75. Sort Colors
 # Solved
 # Medium
