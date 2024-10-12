ef80be2c622dda8b322e3874e2553233ea0803f5
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2024-10-12 15:07:00 -0400

    interchangeable rectangles done fuck this

--- arrays_hashing/medium/2001_number_of_pairs_of_interchangeable_rectangles.py
+++ arrays_hashing/medium/2001_number_of_pairs_of_interchangeable_rectangles.py
@@ -1,3 +1,57 @@
+class Solution:
+    def interchangeableRectangles(self, rectangles: List[List[int]]) -> int:
+      count = {}
+
+      for w, h, in rectangles:
+        count[w / h] = 1 + count.get(w /h, 0)
+      
+      res = 0
+      for c in count.values():
+        if c > 1:
+          res += (c * (c-1)) //2
+      return res
+# neetcode solution
+# create hashmap and map ratio as the key and count as the value
+# iterate through the hashmaps values and if the count is greater than 1 we do math
+#  we check if its greater than 1 because if only one rectangle has that ratio then we cant make pairs+
+
+
+class Solution:
+    def interchangeableRectangles(self, rectangles: List[List[int]]) -> int:
+      ratio = {}
+      count = 0
+
+      for n in rectangles:
+        key = tuple(n)
+        if n[1] != 0:
+          value = n[0] / n[1]
+          ratio[key] = value
+
+      keys = list(ratio.keys())
+
+      for i in range(len(keys)- 1):
+        key = keys[i]
+        val = ratio[key]
+        j = i+ 1
+        while j < len(keys):
+          key2 = keys[j]
+          val2 = ratio[key2]
+          if val == val2:
+            count += 1
+          j += 1
+      return count
+
+
+# my solution that only passed about 8/60 test cases
+# put the rectangles into a hashamp where the dimensions are keys and the ratio are values
+# convert the hashmap back into a list to make it iterable and to extract an index
+# iterate through keys and extract the index to extract the value out of ratio hashmap
+# create j variable that acts as a pointer that slides forward
+# use j and extract key and ratio value just like before and compare the values of i and j
+# increate count if they match and increase j 
+# return j
+
+
 # 2001. Number of Pairs of Interchangeable Rectangles
 # Medium
 # Topics
