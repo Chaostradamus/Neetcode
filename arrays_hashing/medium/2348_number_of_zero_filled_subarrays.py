bb3e8631e4b8a4a1284e42eb86dffd5f9a88ab97
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2024-11-02 15:47:03 -0400

    did medium on my own 2348

--- arrays_hashing/medium/2348_number_of_zero_filled_subarrays.py
+++ arrays_hashing/medium/2348_number_of_zero_filled_subarrays.py
@@ -1,7 +1,74 @@
 class Solution:
-    def numberofarrays():
-        for i in nums:
-            if i 
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        total = 0
+        l ,r = 0, 0
+        count = 0
+
+        while r <= len(nums) -1:
+            if r == len(nums) - 1 and nums[r] == 0:
+                if nums[r] == 0:
+                    count += 1
+                    total += (count * (count + 1)) // 2
+
+            if nums[r] != 0:
+                l = r
+                if count != 0:
+                    total += (count * (count + 1)) // 2
+                    count = 0
+                
+            else:
+                count +=1
+             
+            r += 1
+        return total
+    
+# my solution 0(n) time and space one pass and counts variables
+# my algorithm is to find lengths of subarrays of 0's and to keep that length
+# when the subarray ends i use the sum of natural numbers formulas with that length as X and add to a total
+# i return that total after im done
+# i had to throw boundary checks at the end because i ended once right pointer got to the end but didnt update total if the end was a 0
+# while r in bounds
+# i will first check if nums[r] is a zero
+#       if it is then i will increase count by 1 and add to total
+# this takes care of end of nums list 0's
+# if nums[r] isnt a zero then i will move left to right pointer
+#   then i will check if count isnt 0
+#       if it isnt i will perform the sum of natty numbers formula with count as X and then reset count to 0
+# else i found a zero so i will increase count
+# i will keep moving right pointer throughout each loop
+# return count
+# runtime 97ms and only beats 5.71% and memory is 27.58mb and only beats 5.40%
+
+
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        i, res = 0, 0
+        while i < len(nums):
+            count = 0
+            while i < len(nums) and nums[i] == 0:
+                count +=1
+                res += count
+                i += 1
+
+            i += 1
+        return res
+    
+# neetcodes more optimal solution
+# algo is some mathy stuff where each subarray that increases in length adds length + 1 to the total subarrays in it
+#  subarray of 3 0's will be previous length of 00 plus 3....so at two zeroes it was 3 so we add the current length of 3 to it
+# subarray length examples
+#  1 = 1
+#  2 = 1 + 2
+# 3 = 3 + 3
+# 4 = 6(prev) + 4(current length)
+# 5 = 10(prev) + 5(cur)
+# 6 = 15(previous subarrays) + 6(current length)
+# while i in bounds
+# set count to 0...if we end a subarray and continue on we will have a fresh start
+# then while i in bounds and we are at a 0
+#  we increase count + 1 then add that total and then increase i +=1
+# we increase i outside of the inner while loop to keep the iteration going
+# return res
         
     # 2348. Number of Zero-Filled Subarrays
 # Medium
