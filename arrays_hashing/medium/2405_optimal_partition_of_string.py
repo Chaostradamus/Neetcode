885087b50734df19000a7993304fa559220ef13d
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2024-11-06 19:21:13 -0500

    solo job on 2405

--- arrays_hashing/medium/2405_optimal_partition_of_string.py
+++ arrays_hashing/medium/2405_optimal_partition_of_string.py
@@ -1,6 +1,32 @@
-class SOlution():
-    def __init__(self):
-        pass
+class Solution:
+    def partitionString(self, s: str) -> int:
+        l, r = 0, 1
+        res = []
+        dupes  = {}
+        
+        while r < len(s):
+                if l == r:
+                    r += 1
+
+                dupes[s[l]] = 1 + dupes.get(s[l], 0)
+
+                if s[r] not in dupes:
+                    dupes[s[r]] = 1 + dupes.get(s[r], 0)
+                    r += 1
+                else:
+                    res.append(s[l:r])
+                    l = r
+                    r += 1
+                    dupes = {}
+        return len(res) + 1
+    
+# my algo...one pass and o(n) space
+# my thinking is a sliding window where i add current elements to a hashmap
+# if i find something that is already in the hashmap then i will append the correct substring and update pointers while
+# also resetting the hashmap to clear it out
+# i add one at the end of the return because if the last letter will be its own subarray if it is a duplicate in the subarray before it
+# or the last subarray didnt finish with finding a duplicate and wont be added to res
+# i couldnt figure out how to start from the first element and edge case of the last element 
 
 # 2405. Optimal Partition of String
 # Medium
