533d422f5c97708ecde7eff33f3e4620ed334c24
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2025-02-03 23:36:36 -0500

    added

--- SQL/Joins/1174_immediate_food_delivery_II.sql
+++ SQL/Joins/1174_immediate_food_delivery_II.sql
@@ -1,9 +1,9 @@
 Select 
     round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
-from Delivery
+from Delivery d
 where (customer_id, order_date) in (
   Select customer_id, min(order_date) 
-  from Delivery
+  from Delivery as d
   group by customer_id
 );
 
