7e724bda4c10b80f1d3d66ea656fc7eb11d7e59e
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2025-02-09 02:40:44 -0500

    2356

--- SQL/Joins/550_gameplay_analysis_4.sql
+++ SQL/Joins/550_gameplay_analysis_4.sql
@@ -1,3 +1,13 @@
+select round( count(distinct player_id)/ (select count(distinct player_id) from activity), 2) as fraction
+from activity
+where(player_id, event_date) in (
+    select player_id , date_add(min(event_date), interval 1 day)
+    from activity
+    group by player_id
+    
+)
+
+
 -- 550. Game Play Analysis IV
 -- Medium
 -- Topics
