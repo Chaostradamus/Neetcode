550c336ee1f7132b00ab4a41e3e5bfcaff78c057
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2025-02-24 18:10:43 -0500

    1164 done

--- SQL/Advanced_select_and_joins/1164_product_price_at_a_given_date.sql
+++ SQL/Advanced_select_and_joins/1164_product_price_at_a_given_date.sql
@@ -1,3 +1,21 @@
+select product_id, 10 as price
+from products
+group by product_id
+having min(change_date) > '2019-08-16'
+
+union
+
+select p2.product_id, new_price
+from products p2
+where(p2.product_id, p2.change_date) in (
+    select product_id, max(change_date) as recentDate
+    from products
+    where change_date <= '2019-08-16'
+    group by product_id
+)
+
+
+
 -- 1164. Product Price at a Given Date
 -- Medium
 -- Topics
