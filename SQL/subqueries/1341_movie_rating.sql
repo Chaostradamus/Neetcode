10b5aa717cb55ad31f9e6ff4b0c58c11044c48bf
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2025-03-12 17:40:24 -0400

    602 added

---
+++ SQL/subqueries/1341_movie_rating.sql
@@ -0,0 +1,66 @@
+(select name as results
+from movierating join users using(user_id)
+group by name
+order by count(*) desc, name
+limit 1
+)
+union all
+
+(select title as results
+from movierating join movies using(movie_id)
+where extract(year_month from created_at) = 202002
+group by title
+order by avg(rating) desc, title
+limit 1
+)
+
+-- 1341. Movie Rating
+-- Medium
+-- Topics
+-- Companies
+-- SQL Schema
+-- Pandas Schema
+-- Table: Movies
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | movie_id      | int     |
+-- | title         | varchar |
+-- +---------------+---------+
+-- movie_id is the primary key (column with unique values) for this table.
+-- title is the name of the movie.
+ 
+
+-- Table: Users
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | user_id       | int     |
+-- | name          | varchar |
+-- +---------------+---------+
+-- user_id is the primary key (column with unique values) for this table.
+-- The column 'name' has unique values.
+-- Table: MovieRating
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | movie_id      | int     |
+-- | user_id       | int     |
+-- | rating        | int     |
+-- | created_at    | date    |
+-- +---------------+---------+
+-- (movie_id, user_id) is the primary key (column with unique values) for this table.
+-- This table contains the rating of a movie by a user in their review.
+-- created_at is the user's review date. 
+ 
+
+-- Write a solution to:
+
+-- Find the name of the user who has rated the greatest number of movies. In case of a tie, return the lexicographically smaller user name.
+-- Find the movie name with the highest average rating in February 2020. In case of a tie, return the lexicographically smaller movie name.
+-- The result format is in the following example.
+
+ 
